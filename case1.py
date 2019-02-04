01e82a95bae5e5b13a9e96fe708d523be5b3b310
Dhamodhiran <47314987+Dhamodhiran@users.noreply.github.com>
2019-02-04 00:23:45 -0800

    Create case1.py

---
+++ case1.py
@@ -0,0 +1,8 @@
+# this in an python program,you can enter the number and check its is positive or nrgative or zero
+num=int(input('enter the number:'))
+if num>0:
+	print('Positive')
+elif num==0:
+	print('Zero')
+else:
+	print('Negative')
